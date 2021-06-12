f2b24771086f26269e7329b77a3a581a4f411f62
Err0rBlade <rathod.akshay4321@gmail.com>
2021-06-12 11:25:14 +0530

    init

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { Button, Card, CardTitle, CardSubtitle, CardBody } from "reactstrap";
 import PropTypes from "prop-types";
 import { Redirect } from "react-router-dom";
 import { logout } from "../actions/authActions";
